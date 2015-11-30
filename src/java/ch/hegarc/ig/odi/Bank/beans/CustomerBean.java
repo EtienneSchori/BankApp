91d5995c04bbe9267e321ca1f9d1ab1b7bdcaad6
Etienne <etienne-schori@hotmail.com>
2015-11-30 17:41:20 +0100

    Finalisation du bean CustomerBean
    Etienne

---
+++ src/java/ch/hegarc/ig/odi/Bank/beans/CustomerBean.java
@@ -0,0 +1,47 @@
+package ch.hegarc.ig.odi.Bank.beans;
+
+import ch.hegarc.ig.odi.Bank.service.Services;
+import java.io.Serializable;
+import javax.inject.Inject;
+
+public class CustomerBean implements Serializable {
+
+    @Inject
+    Services services;
+    
+    private Integer number = null;
+    private String lastName = null;
+    private String firstName = null;
+
+    public CustomerBean() {
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String create() {
+        services.saveCustomer(this.number, this.firstName, this.lastName);
+        return "Create";
+    }
+}
